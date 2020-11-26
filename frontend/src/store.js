9df468e37028e10b8f90b55ae7ce983fdf1d9b3b
Eric J <ejuquel@yahoo.fr>
2020-11-26 16:45:50 +0100

    wip register

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,14 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer } from './reducers/usersReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/usersReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
