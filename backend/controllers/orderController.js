b75962fd9b4d9e6516a9173a6e6bbdb3d07428d5
Eric J <ejuquel@yahoo.fr>
2020-12-02 12:01:36 +0100

    fix admin OrderList

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,10 +79,37 @@ export const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc   Update order to delivered
+// @route  PUT /api/orders/:id/deliver
+// @acces  Private/Admin
+export const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
 // @desc   Get logged in user orders
 // @route  GET /api/orders/myorders
 // @acces  Private
 export const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
+})
+
+// @desc   Get all orders
+// @route  GET /api/orders
+// @acces  Private/Admin
+export const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
   res.json(orders)
 })
